f9ddb5f504ccea0e39f885ebf7cf2e95c3889b62
William Line <william.line@proton.me>
2025-04-26 14:55:35 +0100

    fixed deeper in the catacombs socket programming

--- challenges/Cryptography/Deeper in the catacombs/deeperInTheCatacombs.py
+++ challenges/Cryptography/Deeper in the catacombs/deeperInTheCatacombs.py
@@ -2,9 +2,14 @@
 
 import socket
 import random
+import selectors
+import types
+from signal import signal, SIGPIPE, SIG_DFL
+
+signal(SIGPIPE,SIG_DFL)
 
 IP = "127.0.0.1"
-PORT = 5006
+PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_STREAM) # UDP
@@ -167,11 +172,7 @@ def isMillerRabinPassed(mrc):
             return False
     return True
 
-
-if __name__ == '__main__':
-    sock.listen(0)
-    client_socket, client_address = sock.accept()
-
+def generatepq():
     p=0
     q=0
 
@@ -200,34 +201,188 @@ if __name__ == '__main__':
 
         if gcd((p-1)*(q-1),65537)==1:
             coPrime=True
+        return p,q
+                        
 
-        
-                
+userData={}
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
 
-    client_socket.send(" - Generating your public / private key-pairs now . . .\n".encode("utf-8"))
 
-    public, private = generate_key_pair(p, q)
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+            if "message" not in userData[key.data.addr[1]]:
+                message=recv_data.decode("utf-8")
+                #encrypted_msg = encrypt(userData[key.data.addr[1]]["public"][0], message)
+                userData[key.data.addr[1]]["message"]=message
+            elif "encrypted_msg" in userData[key.data.addr[1]]:
+                cipher_text=recv_data.decode("utf-8").strip("\n")
+                if str.isdigit(cipher_text):
+                    cipher_text=int(cipher_text)
+                    userData[key.data.addr[1]]["cipher_text"]=cipher_text
 
-    client_socket.send((f" - Your public key is ({public[0]},{public[1]})\n").encode("utf-8"))
-    if private[0]!=None:
-        flag=encrypt(public,"!FLAG!{RSA_n0_problem}!FLAG! ")
-        client_socket.send((f"\n Here's some cryptography I did earlier: {flag}\n").encode("utf-8"))
-        #message = input(" - Enter a message to encrypt with your public key: ")
-        client_socket.send(" - Enter a message to encrypt with your public key: ".encode("utf-8"))
-        message=client_socket.recv(1024).decode("utf-8")
-        encrypted_msg = encrypt(public, message)
 
-        client_socket.send((f" - Your encrypted message is: {encrypted_msg}\n").encode("utf-8"))
-        
-        #cipher_text=int(input("- Enter some cipher text to decrypt with the private key: "))
-        client_socket.send("- Enter some cipher text to decrypt with the private key: ".encode("utf-8"))
-        cipher_text=int(client_socket.recv(1024).decode("utf-8"))
 
-        if cipher_text==flag:
-            client_socket.send("You're not allowed to do that\n".encode("utf-8"))
         else:
-            client_socket.send((f"- Your decrypted cipher text is: {decrypt(private,cipher_text)}\n").encode("utf-8"))
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        try:
+            if data.outb:
+                print(f"Echoing {data.outb!r} to {data.addr}")
+                sent = sock.send(data.outb)  # Should be ready to write
+                data.outb = data.outb[sent:]
 
-    client_socket.send("============================================ END ==========================================================".encode("utf-8"))
+            if key.data.addr[1] not in userData:
+                sock.send(" - Generating your public / private key-pairs now . . .\n".encode("utf-8"))
+                p, q=generatepq()
+                public, private = generate_key_pair(p, q)
+                userData[key.data.addr[1]]={"public" : [public], "private" : [private]}
+                sock.send((f" - Your public key is ({public[0]},{public[1]})\n").encode("utf-8"))
+                
+                flag=encrypt(public,"!FLAG!{RSA_n0_problem}!FLAG! ")
+                userData[key.data.addr[1]]["flag"]=flag
+                sock.send((f"\n Here's some cryptography I did earlier: {flag}\n").encode("utf-8"))
 
-    client_socket.close()
+                sock.send(" - Enter a message to encrypt with your public key: ".encode("utf-8"))
+            elif "encrypted_msg" not in userData[key.data.addr[1]] and "message" in userData[key.data.addr[1]]:
+                encrypted_msg = encrypt(userData[key.data.addr[1]]["public"][0], message)
+                userData[key.data.addr[1]]["encrypted_msg"]=encrypted_msg
+                sock.send((f" - Your encrypted message is: {userData[key.data.addr[1]]['encrypted_msg']}\n").encode("utf-8"))
+                sock.send("- Enter some cipher text to decrypt with the private key: ".encode("utf-8"))
+                
+            elif "cipher_text" in userData[key.data.addr[1]] and "decrypted_msg" not in userData[key.data.addr[1]]:
+                decrypted_msg=decrypt(userData[key.data.addr[1]]['private'][0],userData[key.data.addr[1]]['cipher_text'])
+                userData[key.data.addr[1]]['decrypted_msg']=decrypted_msg
+
+                if userData[key.data.addr[1]]['cipher_text']==userData[key.data.addr[1]]['flag']:
+                    sock.send("You're not allowed to do that\n".encode("utf-8"))
+                else:
+                    sock.send((f"- Your decrypted cipher text is: {userData[key.data.addr[1]]['decrypted_msg']}\n").encode("utf-8"))
+            
+            elif "decrypted_msg" in userData[key.data.addr[1]]:
+                print(f"Closing connection to {data.addr}")
+                userData.pop(key.data.addr[1])
+                sel.unregister(sock)
+                sock.close()
+
+        except:
+            print(f"Closing connection to {data.addr}")
+            userData.pop(key.data.addr[1])
+             
+
+
+if __name__ == '__main__':
+    sel = selectors.DefaultSelector()
+
+    sock.listen()
+    sock.setblocking(False)
+    sel.register(sock, selectors.EVENT_READ, data=None)
+
+    try:
+        while True:
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    accept_wrapper(key.fileobj)
+                else:
+                    service_connection(key, mask)
+    except KeyboardInterrupt:
+        print("Caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
+
+
+
+
+    while True:
+        sock.listen(0)
+        client_socket, client_address = sock.accept()
+
+        p=0
+        q=0
+
+        coPrime=False
+
+        while coPrime==False:
+            while True:
+                n = 1024
+                prime_candidate = getLowLevelPrime(n)
+                if not isMillerRabinPassed(prime_candidate):
+                    continue
+                else:
+                    #print(n, "bit prime is: \n", prime_candidate)
+                    p=prime_candidate
+                    break
+
+            while True:
+                n = 1024
+                prime_candidate = getLowLevelPrime(n)
+                if not isMillerRabinPassed(prime_candidate):
+                    continue
+                else:
+                    #print(n, "bit prime is: \n", prime_candidate)
+                    q=prime_candidate
+                    break
+
+            if gcd((p-1)*(q-1),65537)==1:
+                coPrime=True
+
+            #up to here
+                    
+
+        client_socket.send(" - Generating your public / private key-pairs now . . .\n".encode("utf-8"))
+
+        public, private = generate_key_pair(p, q)
+
+        client_socket.send((f" - Your public key is ({public[0]},{public[1]})\n").encode("utf-8"))
+
+        messageRecieved=False
+
+        if private[0]!=None:
+            flag=encrypt(public,"!FLAG!{RSA_n0_problem}!FLAG! ")
+            client_socket.send((f"\n Here's some cryptography I did earlier: {flag}\n").encode("utf-8"))
+
+            message=""
+            while not messageRecieved:
+            #message = input(" - Enter a message to encrypt with your public key: ")
+                client_socket.send(" - Enter a message to encrypt with your public key: ".encode("utf-8"))
+                message=client_socket.recv(1024).decode("utf-8")
+                if message!='':
+                    client_socket.send(" - Enter text".encode("utf-8"))
+                    messageRecieved=True
+
+            encrypted_msg = encrypt(public, message)
+
+            client_socket.send((f" - Your encrypted message is: {encrypted_msg}\n").encode("utf-8"))
+
+            #cipher_text=int(input("- Enter some cipher text to decrypt with the private key: "))
+            messageRecieved=False
+            while not messageRecieved:
+                client_socket.send("- Enter some cipher text to decrypt with the private key: ".encode("utf-8"))
+                cipher_text=client_socket.recv(1024).decode("utf-8")
+                if isinstance(cipher_text, int):
+                    cipher_text=int(cipher_text)
+                    messageRecieved=True
+                else:
+                    client_socket.send(" - Must enter numbers".encode("utf-8"))
+
+            if cipher_text==flag:
+                client_socket.send("You're not allowed to do that\n".encode("utf-8"))
+            else:
+                client_socket.send((f"- Your decrypted cipher text is: {decrypt(private,cipher_text)}\n").encode("utf-8"))
+
+        client_socket.send("============================================ END ==========================================================".encode("utf-8"))
+
+        client_socket.close()
