592dcefeb53727505e78dc71df5ab09123d7e570
William Line <william.line@proton.me>
2025-05-26 21:03:14 +0100

    added to bash script

--- domainName.sh
+++ domainName.sh
@@ -3,7 +3,9 @@
 wget -O /etc/yum.repos.d/snapd.repo https://bboozzoo.github.io/snapd-amazon-linux/al2023/snapd.repo
 dnf install snapd -y
 
+sleep 5
 snap install core
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
 certbot --nginx
+echo "hi"
