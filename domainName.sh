dbc0ced94f160c92df22645a77864b8a0d9d1e46
William Line <william.line@proton.me>
2025-06-03 15:56:11 +0100

    changed config again

--- domainName.sh
+++ domainName.sh
@@ -2,12 +2,15 @@
 
 wget -O /etc/yum.repos.d/snapd.repo https://bboozzoo.github.io/snapd-amazon-linux/al2023/snapd.repo
 dnf install snapd -y
+echo snapd installed
 
 sleep 5
 snap install core
 snap install --classic certbot
+echo certbot installed
 ln -s /snap/bin/certbot /usr/bin/certbot
-certbot --nginx
+printf "\nY\ninvoluntaryctf.net\n" | certbot --nginx
 printf "\n"
 printf "Y"
 printf "involuntaryctf.net"
+echo domain name set up
