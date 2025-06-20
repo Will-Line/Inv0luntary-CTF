80285d61feee835b93aad83f20e9f5496e156092
William Line <william.line@proton.me>
2025-06-21 00:19:59 +0100

    fixed a.out and goodform.sh

--- challenges/Reverse engineering/In good form/ingoodform.sh
+++ challenges/Reverse engineering/In good form/ingoodform.sh
@@ -4,7 +4,7 @@ number=$(netstat -ano | grep 0.0.0.0:$1 | wc -l)
 
 echo $number
 if [[ $number -lt 3 ]]; then
-    ncat -l involuntaryctf.net -p $1 -c '/var/app/current/challenges/Reverse\ engineering/In\ good\ form/a.out' &
+    ncat -l -p $1 -c '/var/app/current/challenges/Reverse\ engineering/In\ good\ form/a.out' &
     echo "restarted";
 else
     echo "still running"
